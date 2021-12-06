cae6b76d9d678d6eedd045a219f8af31df5659d3
Gvrarnzs <arianzas1997@gmail.com>
2021-12-06 22:00:35 +0700

    PJT-8 LandingPage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import "tailwindcss/tailwind.css"
 
+import { Routes, Route, Navigate } from 'react-router-dom'
+import LandingPage from './pages/LandingPage';
+
 function App() {
+
+  
+
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="bg-white">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link bg-white"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1 className="bg-black">TEST</h1>
-      <h1>TEST</h1>
+    <div >
+      <Routes>
+        <Route path={'/'} element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
