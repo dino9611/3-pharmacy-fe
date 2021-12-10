97761220e17adeff9ae9b5a91b542c9d233cbdfb
Hilmawan Zaky <hilmawanzaky57@gmail.com>
2021-12-10 10:41:08 +0700

    fix-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,26 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      green: {
-        light: "#b4c4a6",
-        dark: '#66806a'
-      },
-      peach: {
-        light: "#ffc286",
-        dark: '#fff1af'
-      },
-      grey: {
-        light: "#D1D5DB",
-        dark: "#9CA3AF"
-      },
-      light: {
-        light: "#ffffff",
-        dark: "#000000"
+    extend: {
+      colors: {
+        green: {
+          light: "#b4c4a6",
+          dark: '#66806a'
+        },
+        peach: {
+          light: "#ffc286",
+          dark: '#fff1af'
+        },
+        grey: {
+          light: "#D1D5DB",
+          dark: "#9CA3AF"
+        },
+        light: {
+          light: "#ffffff",
+          dark: "#000000"
+        }
       }
-    }
+    },
   },
   variants: {
     extend: {},
