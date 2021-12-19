483fb23118f0a947e1790e63cef5c11ef591d4f6
rachmann0 <reecedarby1889@gmail.com>
2021-12-19 21:02:24 +0700

    potential revenue

--- src/redux/actions/revenueActions.js
+++ src/redux/actions/revenueActions.js
@@ -19,16 +19,60 @@ export const resetState = (propName) => {
 
 const DEBOUNCE_DELAY = 100;
 
+// ! READ
 let getRevenue_timeoutID;
-export const getRevenue = (handleResult = {}) => {
+export const getRevenue = (params, handleResult = {}) => {
   return (dispatch, getState, API_URL) => {
     const { handleSuccess, handleFail, handleFinally } = handleResult;
     clearTimeout(getRevenue_timeoutID);
 
     getRevenue_timeoutID = setTimeout(async () => {
+      let { yearMonthStart, yearMonthEnd } = params;
       try {
-        const [result] = axios.get(API_URL + `/revenue`);
-        dispatch(setState('revenue', result));
+        // add one day
+        yearMonthStart.setDate(yearMonthStart.getDate() + 1);
+        yearMonthEnd.setDate(yearMonthEnd.getDate() + 1);
+        // turn date to ISO string
+        yearMonthStart = yearMonthStart.toISOString();
+        yearMonthEnd = yearMonthEnd.toISOString();
+        const { data } = await axios.get(
+          API_URL +
+            `/stats/revenue?yearMonthStart=${yearMonthStart}&yearMonthEnd=${yearMonthEnd}`
+        );
+        dispatch(setState('revenue', data));
+        handleSuccess !== undefined && handleSuccess();
+      } catch (error) {
+        handleFail !== undefined && handleFail(error);
+      }
+      handleFinally !== undefined && handleFinally();
+    }, DEBOUNCE_DELAY);
+  };
+};
+
+let getPotentialRevenue_timeoutID;
+export const getPotentialRevenue = (params, handleResult = {}) => {
+  return (dispatch, getState, API_URL) => {
+    const { handleSuccess, handleFail, handleFinally } = handleResult;
+    clearTimeout(getPotentialRevenue_timeoutID);
+
+    getPotentialRevenue_timeoutID = setTimeout(async () => {
+      let { yearMonthStart, yearMonthEnd } = params;
+      try {
+        // yearMonthStart.setHours(0, 0, 0, 0);
+
+        // add one day
+        yearMonthStart.setDate(yearMonthStart.getDate() + 1);
+        yearMonthEnd.setDate(yearMonthEnd.getDate() + 1);
+        // turn date to ISO string
+        yearMonthStart = yearMonthStart.toISOString();
+        yearMonthEnd = yearMonthEnd.toISOString();
+        // console.log(yearMonthStart);
+        // console.log(yearMonthEnd);
+        const { data } = await axios.get(
+          API_URL +
+            `/stats/potential_revenue?yearMonthStart=${yearMonthStart}&yearMonthEnd=${yearMonthEnd}`
+        );
+        dispatch(setState('potentialRevenue', data));
         handleSuccess !== undefined && handleSuccess();
       } catch (error) {
         handleFail !== undefined && handleFail(error);
