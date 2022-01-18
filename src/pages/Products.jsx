a2daee47f9da01be9254bdff96fd37682a978882
Hilmawan Zaky <hilmawanzaky57@gmail.com>
2022-01-18 10:07:11 +0700

    fix card

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -65,48 +65,52 @@ const Products = () => {
       return (
         <div
           key={index + 1}
-          className='m-2 bg-white rounded-md overflow-hidden p-4 shadow-md'
+          className='m-2 bg-white rounded-md overflow-hidden p-4 shadow-md flex flex-col justify-between'
         >
-          <img
-            src={API_URL + val.imagePath}
-            alt=''
-            className='object-contain w-48 h-48 phone:w-full phone:h-24 mx-auto bg-gray-200 rounded-md mb-4'
-          />
           <div>
-            <p
-              className='poppins text-primary1 font-bold text-lg phone:text-sm hover:text-lightblue cursor-pointer mb-4 phone:mb-1'
-              onClick={() => productDetailsHandler(val.id)}
-            >
-              {capitalize(val.productName)}
-            </p>
-            <p className='poppins text-gray-600 font-bold text-sm mb-2 phone:mb-1 phone:text-xs'>
-              {toRupiah(val.productPriceRp)}
-            </p>
-            <p className='poppins text-gray-400 text-sm phone:text-xs'>
-              Stock {val.stock}
-            </p>
+            <img
+              src={API_URL + val.imagePath}
+              alt=''
+              className='object-contain w-48 h-48 phone:w-full phone:h-24 mx-auto bg-gray-200 rounded-md mb-4'
+            />
+            <div>
+              <p
+                className='poppins text-primary1 font-bold text-lg phone:text-sm hover:text-lightblue cursor-pointer mb-4 phone:mb-1'
+                onClick={() => productDetailsHandler(val.id)}
+              >
+                {capitalize(val.productName)}
+              </p>
+              <p className='poppins text-gray-600 font-bold text-sm mb-2 phone:mb-1 phone:text-xs'>
+                {toRupiah(val.productPriceRp)}
+              </p>
+              <p className='poppins text-gray-400 text-sm phone:text-xs'>
+                Stock {val.stock}
+              </p>
+            </div>
           </div>
-          <hr className='my-4 phone:my-2 border' />
-          <button
-            className='text-primary1 text-base font-bold hover:text-lightblue flex items-center phone:text-xs'
-            onClick={() => addToCart(index)}
-          >
-            <svg
-              className='w-5 h-5 phone:w-4 phone:h-4 mr-1'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
+          <div>
+            <hr className='my-4 phone:my-2 border' />
+            <button
+              className='text-primary1 text-base font-bold hover:text-lightblue flex items-center phone:text-xs'
+              onClick={() => addToCart(index)}
             >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-              />
-            </svg>
-            Add to cart
-          </button>
+              <svg
+                className='w-5 h-5 phone:w-4 phone:h-4 mr-1'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                />
+              </svg>
+              Add to cart
+            </button>
+          </div>
         </div>
       );
     });
